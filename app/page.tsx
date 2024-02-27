6c160ba90ec1d6302eb6dd79e101fd23844b9eea
Fwaad Ahmad <68646332+fwaadahmad1@users.noreply.github.com>
2024-02-27 18:35:20 -0500

    updated styles for content pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-start justify-center p-24">
-    </main>
+    <div className="flex flex-col items-start justify-center p-24">
+    </div>
   );
 }
