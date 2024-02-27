369987f6f2455f85548a793fad3d56d9d3b99e78
Fwaad Ahmad <68646332+fwaadahmad1@users.noreply.github.com>
2024-02-27 18:31:17 -0500

    updated folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-start justify-start p-24">
-      Hello
+    <main className="flex min-h-screen flex-col items-start justify-center p-24">
     </main>
   );
 }
--- components/navigation-menu.tsx
+++ components/navigation-menu.tsx
@@ -1,68 +1,74 @@
 "use client"
-
-import Link from "next/link"
-import {LucideIcon} from "lucide-react"
-
 import {cn} from "@/lib/utils"
-import {
-    Tooltip, TooltipContent, TooltipTrigger,
-} from "@/components/ui/tooltip"
-import {buttonVariants} from "@/components/ui/button";
+import {Button} from "@/components/ui/button"
+import Image from "next/image";
+import Logo from "@/public/logo.svg"
+import {LayoutDashboard, Notebook, Package, ShoppingBag} from "lucide-react";
+import {ReactNode} from "react";
+import {useRouter, usePathname} from "next/navigation";
 
-interface NavProps {
-    isCollapsed: boolean
-    links: {
-        title: string
-        label?: string
-        icon: LucideIcon
-        variant: "default" | "ghost"
-    }[]
+type NavLink = {
+    title: string, path: string
 }
 
-export function Nav({links, isCollapsed}: NavProps) {
-    return (<div
-        data-collapsed={isCollapsed}
-        className="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2"
-    >
-        <nav
-            className="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
-            {links.map((link, index) => {
-                if (isCollapsed) {
-                    return <Tooltip key={index} delayDuration={0}>
-                        <TooltipTrigger asChild>
-                            <Link
-                                href="#"
-                                className={cn(buttonVariants({variant: link.variant, size: "icon"}),
-                                              "h-9 w-9",
-                                              link.variant === "default" && "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white")}
-                            >
-                                <link.icon className="h-4 w-4"/>
-                                <span className="sr-only">{link.title}</span>
-                            </Link>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" className="flex items-center gap-4">
-                            {link.title}
-                            {link.label && <span className="ml-auto text-muted-foreground">{link.label}</span>}
-                        </TooltipContent>
-                    </Tooltip>
-                } else {
-                    return <Link
-                        key={index}
-                        href="#"
-                        className={cn(buttonVariants({variant: link.variant, size: "sm"}),
-                                      link.variant === "default" && "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
-                                      "justify-start")}
-                    >
-                        <link.icon className="mr-2 h-4 w-4"/>
-                        {link.title}
-                        {link.label &&
-                            <span
-                                className={cn("ml-auto",
-                                              link.variant === "default" && "text-background dark:text-white")}
-                            >{link.label}</span>}
-                    </Link>
-                }
-            })}
+export function Sidebar({className}: { className: string }) {
+
+    const router = useRouter();
+    const path = usePathname();
+
+    console.log(path)
+
+    function NavLink({children, navLink}: { children: ReactNode, navLink: NavLink }) {
+        return <Button variant={path.includes(`${navLink.path}`) ? "secondary" : "ghost"}
+                       className="w-full justify-start pr-24"
+                       onClick={() => router.push(navLink.path)}>
+            {children}
+        </Button>
+    }
+
+    const navLinks: Array<NavLink> = [
+        {
+            title: "Dashboard",
+            path: "/dashboard"
+        }, {
+            title: "Orders",
+            path: "/orders"
+        }, {
+            title: "Product",
+            path: "/product"
+        }, {
+            title: "Inventory",
+            path: "/inventory"
+        }
+    ]
+
+    return (
+        <nav className={cn("max-w-min pb-12 bg-card", className)}>
+            <div className="space-y-4 py-4">
+                <div className="px-3 py-2">
+                    <div className={cn("flex justify-center py-8")}>
+                        <Image src={Logo} alt={"CandyCo"}/>
+                    </div>
+                    <div className="space-y-1">
+                        <NavLink navLink={navLinks[0]}>
+                            <LayoutDashboard className="mr-2 h-4 w-4"/>
+                            {navLinks[0].title}
+                        </NavLink>
+                        <NavLink navLink={navLinks[1]}>
+                            <ShoppingBag className="mr-2 h-4 w-4"/>
+                            {navLinks[1].title}
+                        </NavLink>
+                        <NavLink navLink={navLinks[2]}>
+                            <Package className="mr-2 h-4 w-4"/>
+                            {navLinks[2].title}
+                        </NavLink>
+                        <NavLink navLink={navLinks[3]}>
+                            <Notebook className="mr-2 h-4 w-4"/>
+                            {navLinks[3].title}
+                        </NavLink>
+                    </div>
+                </div>
+            </div>
         </nav>
-    </div>)
+    )
 }
