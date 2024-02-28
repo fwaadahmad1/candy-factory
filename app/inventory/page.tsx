f025f95b5bf357218f4800ac780eeacd1f0e3fee
Smitpatel2801 <smiteducation2801@gmail.com>
2024-02-28 00:35:35 -0500

    added in Line

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -31,7 +31,7 @@ const InventoryPage = () => {
                 <Search className="absolute left-2.5 h-4 w-4 text-muted-foreground" />
                 <Input placeholder="Your search..." className="rounded-full pl-8" />
               </div>
-              <Button className={"ml-16"}>Add Item</Button>
+              <Button variant={"secondary"}>Add Item</Button>
             </CardContent>
           </Card>
     
