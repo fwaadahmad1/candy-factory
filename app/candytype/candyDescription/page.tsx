375911ba631d02cb13b518c807ac79d9cbcc4053
DawarWaqar <dawarwaqar90@gmail.com>
2024-03-30 03:38:18 -0400

    units in candy descr complete

--- app/candytype/candyDescription/page.tsx
+++ app/candytype/candyDescription/page.tsx
@@ -65,7 +65,6 @@ function Page() {
   const {data} = useGetCandyTypeQuery({});
   const {data:settings} = useGetAddSettingsQuery({})
   const settingsObj = processSettings(settings);
-  console.log("settings: ",settingsObj)
   const orderDetails : candyTypeData[]  = data ?? [];
   const searchParams2 = useSearchParams();
   const search = searchParams2.get('candyName');
@@ -103,11 +102,11 @@ function Page() {
 
               <div className={"!mt-0"}>
                 {/* <h1 className={"text-xl font-extrabold"}>Estimated time:</h1> */}
-                <text
+                <span
                   className={"text-blue-500 text-lg tracking-wide font-semibold"}
                 >
                   {toHoursAndMinutes(order.total_time)}
-                </text>
+                </span>
               </div>
             </CardHeader>
 
@@ -181,7 +180,7 @@ function Page() {
                         <TableRow>
                           <TableHead>Setting</TableHead>
                           <TableHead>Value</TableHead>
-                          {/* <TableHead>Unit</TableHead> */}
+                          <TableHead>Unit</TableHead>
                         </TableRow>
                       </TableHeader>
                       <TableBody>
@@ -192,7 +191,7 @@ function Page() {
                               <TableCell>
                                 {`${JSON.parse(order.quantity_mixer_settings)[i]}`}
                               </TableCell>
-                              {/* <TableCell>{setting}</TableCell> */}
+                              <TableCell>{settingsObj[setting]}</TableCell>
                             </TableRow>
                           );
                         })}
@@ -232,6 +231,7 @@ function Page() {
                         <TableRow>
                           <TableHead>Setting</TableHead>
                           <TableHead>Value</TableHead>
+                          <TableHead>Unit</TableHead>
                         </TableRow>
                       </TableHeader>
                       <TableBody>
@@ -242,6 +242,7 @@ function Page() {
                               <TableCell>
                                 {`${JSON.parse(order.quantity_cooker_settings)[i]}`}
                               </TableCell>
+                              <TableCell>{settingsObj[setting]}</TableCell>
                             </TableRow>
                           );
                         })}
@@ -281,6 +282,7 @@ function Page() {
                         <TableRow>
                           <TableHead>Setting</TableHead>
                           <TableHead>Value</TableHead>
+                          <TableHead>Unit</TableHead>
                         </TableRow>
                       </TableHeader>
                       <TableBody>
@@ -291,6 +293,7 @@ function Page() {
                               <TableCell>
                                 {`${JSON.parse(order.quantity_extruder_settings)[i]}`}
                               </TableCell>
+                              <TableCell>{settingsObj[setting]}</TableCell>
                             </TableRow>
                           );
                         })}
@@ -330,6 +333,7 @@ function Page() {
                         <TableRow>
                           <TableHead>Setting</TableHead>
                           <TableHead>Value</TableHead>
+                          <TableHead>Unit</TableHead>
                         </TableRow>
                       </TableHeader>
                       <TableBody>
@@ -340,6 +344,7 @@ function Page() {
                               <TableCell>
                                 {`${JSON.parse(order.quantity_packaging_settings)[i]}`}
                               </TableCell>
+                              <TableCell>{settingsObj[setting]}</TableCell>
                             </TableRow>
                           );
                         })}
